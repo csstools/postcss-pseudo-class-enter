f0c362a9a6efc9c808f3db54c26651c8ba43bc52
Ivan Nikolić <niksy5@gmail.com>
2020-10-20 16:09:33 +0200

    Add support for PostCSS 8

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import postcss from 'postcss';
 import parser from 'postcss-selector-parser';
 
-export default postcss.plugin('postcss-pseudo-class-enter', opts => {
+const plugin = opts => {
 	// add conditional prefix
 	const pseudoClass = `:${Object(opts).prefix ? `-${opts.prefix}-` : ''}enter`;
 	const pseudoClassRegExp = new RegExp(pseudoClass, 'i');
@@ -9,33 +8,39 @@ export default postcss.plugin('postcss-pseudo-class-enter', opts => {
 	// add outline declaration
 	const shouldUseOutline = Object(opts).outline !== null && Object(opts).outline !== undefined;
 
-	return root => {
-		// transform :enter selectors to :focus and :hover
-		root.walkRules(pseudoClassRegExp, rule => {
-			const originalSelector = rule.selector;
+	return {
+		postcssPlugin: 'postcss-pseudo-class-enter',
+		Once: root => {
+			// transform :enter selectors to :focus and :hover
+			root.walkRules(pseudoClassRegExp, rule => {
+				const originalSelector = rule.selector;
 
-			const modifiedSelector = parser(selectors => {
-				transform(selectors, pseudoClass);
-			}).processSync(originalSelector);
+				const modifiedSelector = parser(selectors => {
+					transform(selectors, pseudoClass);
+				}).processSync(originalSelector);
 
-			if (originalSelector !== modifiedSelector) {
-				rule.selector = modifiedSelector;
-			}
-
-			if (shouldUseOutline) {
-				// check for an existing outline declaration
-				const hasOutline = rule.nodes.some(decl => decl.prop === 'outline');
-
-				// if an outline declaration does not exist, prepend it
-				if (!hasOutline) {
-					const outlineValue = !opts.outline || opts.outline === true ? 0 : opts.outline;
-
-					rule.prepend({ prop: 'outline', value: outlineValue });
+				if (originalSelector !== modifiedSelector) {
+					rule.selector = modifiedSelector;
 				}
-			}
-		});
+
+				if (shouldUseOutline) {
+					// check for an existing outline declaration
+					const hasOutline = rule.nodes.some(decl => decl.prop === 'outline');
+
+					// if an outline declaration does not exist, prepend it
+					if (!hasOutline) {
+						const outlineValue = !opts.outline || opts.outline === true ? 0 : opts.outline;
+
+						rule.prepend({ prop: 'outline', value: outlineValue });
+					}
+				}
+			});
+		}
 	};
-});
+}
+plugin.postcss = true;
+
+export default plugin;
 
 function transform(selectors, pseudoClass) {
 	selectors.walk(selector => {
